8891d4eda135772c561c89ce7ba2639448ffef68
Ata <codewithata@gmail.com>
2026-06-09 15:59:33 +0300

    feat: add color.h

---
+++ include/color.h
@@ -0,0 +1,14 @@
+#ifndef CSTRIKE_COLOR_H
+#define CSTRIKE_COLOR_H
+
+#define RESET   "\033[0m"
+#define RED     "\033[31m"
+#define GREEN   "\033[32m"
+#define YELLOW  "\033[33m"
+#define BLUE    "\033[34m"
+#define CYAN    "\033[36m"
+#define WHITE   "\033[37m"
+#define BOLD    "\033[1m"
+
+
+#endif //CSTRIKE_COLOR_H
